5d97cbd5e625152453edba94dc760636397a72b0
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-09-05 16:46:18 +0300

    Добавил приветственное сообещние

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -26,11 +27,28 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
+        logger.info("Processing update: {}", updates);
+        for (Update update : updates) {
+            String massageText = update.message().text();
+            long chatId = update.message().chat().id();
+            if (massageText.equals("/start")) {
+                startCommandReceived(chatId, update.message().chat().firstName());
+            } else {
+                sendMessage(chatId, "Sorry, command was not recognized");
+            }
+        }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + ", let's start!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
     }
 
 }
